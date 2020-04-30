facb340226386039267419bd1fe86cd529d91cdc
pablitoierino <63565358+pablitoierino@users.noreply.github.com>
2020-04-30 18:04:34 -0300

    Add files via upload

---
+++ 01 suma/suma.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a,b,s=0;
+    cout<< "ingrese numero entero: "<<endl;cin>> a;
+    cout<<"ingrese otro numero entero: "<<endl; cin>>b;
+s = a+b;
+cout<<"la suma es: "<<s<<endl;
+system ("pause");
+return 0;
+
+}
